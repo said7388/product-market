91716df4251f5cbc669d64f3230b67d137375816
said7388 <mdabusaidriyaz@gmail.com>
2023-04-30 00:56:56 +0600

    create product view page

--- src/pages/product-view.tsx
+++ src/pages/product-view.tsx
@@ -1,6 +1,6 @@
 // @flow strict
 
-import { Container } from "@mui/material";
+import { Container, Rating } from "@mui/material";
 import * as React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -22,7 +22,26 @@ function ProductView() {
   return (
     <Container className='w-full h-full mt-5 grid grid-cols-2'>
       <img src={product?.image} alt='' className='w-9/12' />
-      <div className=''></div>
+      <div className=''>
+        <p className='text-xl font-medium'>{product?.category}</p>
+        <h1 className='text-2xl font-medium'>{product?.title}</h1>
+        <Rating
+          name='read-only'
+          size='medium'
+          value={product?.rating}
+          precision={0.1}
+          readOnly
+        />
+        <p className='text-2xl font-bold'>$ {product?.price}</p>
+        <div className='flex gap-2 m-0 items-center'>
+          {product?.content === "PCVR" ? (
+            <span className='w-4 h-4 rounded-full bg-[#3CD4F5]'></span>
+          ) : (
+            <span className='w-4 h-4 rounded-full bg-[#75DE73]'></span>
+          )}
+          <p className='m-0 text-sm'>{product?.type} Only</p>
+        </div>
+      </div>
     </Container>
   );
 }
